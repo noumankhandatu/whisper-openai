a290c0f447b032174985976a3ef1258e77b568d2
noumankhan <noumankhan>
2023-03-18 18:09:51 +0500

    finishing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
-import openai from "openai";
 import Navbar from "./components/navbar";
 import OpenAiImage from "./assets/openAI.png";
 import "./App.css";
 import { Box } from "@mui/system";
-import { Button, Typography } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { AudioRecorder } from "react-audio-voice-recorder";
 import { audioCss, boxLayout } from "./components/navbar/style";
 
 // static data
 const textColor = "white";
-const backgroundColor = "#0B0F19";
 const textOne = "Whisper Demo";
 const textTwo =
   " Audio transcription and translation demo based on OpenAI Whisper. Record speech from your device to transcribe and translate it (remember to press stop recording when done!).";
@@ -18,19 +16,45 @@ const textTwo =
 function App() {
   // audio rec start
   const [audioUrl, setAudioUrl] = useState(null);
+  const [transcript, setTranscript] = useState(null);
 
-  const addAudioElement = (blob) => {
+  const addAudioElement = async (blob) => {
     const url = URL.createObjectURL(blob);
     setAudioUrl(url);
+
+    const formData = new FormData();
+    const fileType = "audio/mp3";
+    const fileName = "audio_file.mp3";
+    const file = new File([blob], fileName, { type: fileType });
+    formData.append("file", file);
+    formData.append("model", "whisper-1");
+    formData.append("response_format", "text");
+
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/audio/transcriptions",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer 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`,
+          },
+          body: formData,
+        }
+      );
+      const text = await response.text();
+      setTranscript(text);
+    } catch (error) {
+      console.error(error);
+    }
   };
   // audio rec end
 
   // clear button
   const handleClear = () => {
     setAudioUrl(null);
+    setTranscript(null);
   };
 
-  console.log(audioUrl, "audio url");
   return (
     <div className="App">
       <Navbar />
@@ -78,82 +102,40 @@ function App() {
             )}
           </Box>
         </Box>
-        <Box sx={audioCss}>Hello there how are you</Box>
+        <Box sx={audioCss}>
+          {audioUrl ? (
+            <p>
+              {transcript ? transcript : <CircularProgress color="success" />}
+            </p>
+          ) : (
+            `I can write your words 😊 `
+          )}
+        </Box>
       </Box>
     </div>
   );
 }
 
 export default App;
-const RecordAudioButton = () => {
-  const [recording, setRecording] = useState(false);
+// const RecordAudioButton = () => {
+//   const [recording, setRecording] = useState(false);
 
-  const handleClick = () => {
-    setRecording(!recording);
-  };
-
-  return (
-    <Button size="small" variant="contained" onClick={handleClick}>
-      {recording ? (
-        <div className="beeping-animation beeping"></div>
-      ) : (
-        <div className="stop-beeping-animation"></div>
-      )}
-      <p style={{ color: "transparent" }}>..</p>
-
-      <p style={{ marginTop: "2px" }}>
-        {recording ? "Stop Recording" : "Start Recording"}
-      </p>
-    </Button>
-  );
-};
-
-// test this on codesandbox
-//   import "./styles.css";
-// import React, { useState } from "react";
-// import ReactDOM from "react-dom/client";
-// import { AudioRecorder } from "react-audio-voice-recorder";
-// import { Box } from "@mui/material";
-
-// export default function App() {
-//   const [audioUrl, setAudioUrl] = useState(null);
-
-//   const addAudioElement = (blob) => {
-//     const url = URL.createObjectURL(blob);
-//     console.log(url, "Hello");
-//     setAudioUrl(url);
-
-//     const requestOptions = {
-//       method: "POST",
-//       headers: {
-//         Authorization: "Bearer YOUR_API_KEY",
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify({
-//         file: "audio.mp3",
-//         model: "whisper-1"
-//       })
-//     };
-
-//     const z = fetch(
-//       "https://api.openai.com/v1/audio/transcriptions",
-//       requestOptions
-//     )
-//       .then((response) => response.json())
-//       .then((data) => console.log(data));
-
-//     console.log(z);
+//   const handleClick = () => {
+//     setRecording(!recording);
 //   };
 
 //   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>{" "}
-//       <Box sx={{ p: 2, backgroundColor: "#1F2937" }}>
-//         asd
-//         <AudioRecorder onRecordingComplete={addAudioElement} />
-//         {audioUrl && <audio src={audioUrl} controls={true} />}
-//       </Box>
-//     </div>
+//     <Button size="small" variant="contained" onClick={handleClick}>
+//       {recording ? (
+//         <div className="beeping-animation beeping"></div>
+//       ) : (
+//         <div className="stop-beeping-animation"></div>
+//       )}
+//       <p style={{ color: "transparent" }}>..</p>
+
+//       <p style={{ marginTop: "2px" }}>
+//         {recording ? "Stop Recording" : "Start Recording"}
+//       </p>
+//     </Button>
 //   );
-// }
+// };
